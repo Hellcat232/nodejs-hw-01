b7f48ac7706c36df0cb51bf78a02006e294e7620
Hellcat232 <maxkuich1993@gmail.com>
2024-05-25 03:27:08 +0300

    start

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,13 @@
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {};
+
+await generateContacts(5);
+
+// export const createFakeContact = () => ({
+//   name: faker.person.fullName(),
+//   phone: faker.phone.number(),
+//   email: faker.internet.email(),
+//   job: faker.person.jobTitle(),
+// });
